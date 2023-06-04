7697f6624fcb8422510b20909724e172240700b9
kmceasdf <135545760+kmceasdf@users.noreply.github.com>
2023-06-05 02:29:04 +0530

    Create experiment7.js

---
+++ experiment7.js
@@ -0,0 +1,8 @@
+// npx create-react-app app1
+// delete all in src except App.js, index.js
+// in index.js keep only react, reactdom and app imports delete others
+// delete all in public except index.html
+
+// to host the app type : npm start
+
+//
