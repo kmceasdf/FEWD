02c485884d7cf732109b7d9035770ce348483ed4
kmceasdf <135545760+kmceasdf@users.noreply.github.com>
2023-06-05 02:32:01 +0530

    Update experiment7.js

--- experiment7.js
+++ experiment7.js
@@ -5,4 +5,64 @@
 
 // to host the app type : npm start
 
-//
+
+//App.js
+import useContextEx from "./UseContextEx.js"
+
+function App() {
+  return (
+    <div className="App">
+      <useContextEx/>
+    </div>
+  );
+}
+export default App;
+---------------------------------------------------------------
+
+//index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+-----------------------------------------------------------------------
+//useContextEx.js
+import React, { useContext } from 'react';
+
+const theme = {
+    light : {background: "#fff"},
+    dark: {background: "#000"}
+}
+
+const ThemeContext = React.createContext(theme.light)
+
+function useContextEx(props) {
+  return (
+    <div>
+        {}
+        <ThemeContext.Provider value={theme.dark}>
+                <Body />
+        </ThemeContext.Provider>
+    </div>
+  );
+}
+
+function Body(){
+    const theme = useContext(ThemeContext);
+    return(
+        <main
+            style={{background : theme.background, height: "50vh", color: "#fff" }}
+        >
+            I am the main display styled by context!
+        </main>
+    )
+}
+export default useContextEx;
+
+
+
