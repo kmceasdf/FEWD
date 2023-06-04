47a71070201050f8710135b3abc68eb8186b897c
kmceasdf <135545760+kmceasdf@users.noreply.github.com>
2023-06-05 02:28:11 +0530

    Update experiment6.js

--- experiment6.js
+++ experiment6.js
@@ -1,3 +1,10 @@
+// npx create-react-app app1
+// delete all in src except App.js, index.js
+// in index.js keep only react, reactdom and app imports delete others
+// delete all in public except index.html
+
+// to host the app type : npm start
+
 //form.js
 import React, { useState } from 'react';
 
@@ -42,3 +49,31 @@ function Form() {
 }
 
 export default Form;
+
+
+//App.js
+
+import Form from "./Form.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
+
+
+//index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
