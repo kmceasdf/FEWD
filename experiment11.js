3a403a91df630b60578b8027c334c4c6038b928f
kmceasdf <135545760+kmceasdf@users.noreply.github.com>
2023-06-05 08:02:04 +0530

    Create experiment11.js

---
+++ experiment11.js
@@ -0,0 +1,37 @@
+// npx create-react-app app1
+// delete all in src except App.js, index.js
+// in index.js keep only react, reactdom and app imports delete others
+// delete all in public except index.html
+
+// to host the app type : npm start
+------------------------------------------------------------
+
+//App.js
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
