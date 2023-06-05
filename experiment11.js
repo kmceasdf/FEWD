b63a3fc46c70357aba365145d51ec0dea79023cf
kmceasdf <135545760+kmceasdf@users.noreply.github.com>
2023-06-05 08:03:34 +0530

    Update experiment11.js

--- experiment11.js
+++ experiment11.js
@@ -35,3 +35,15 @@ function App() {
 }
 
 export default App;
+-----------------------------------------------------------
+//index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
